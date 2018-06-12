b6773d7d5d8287c3d929f02fb9cb7859c6beef36
tianzhongqiu <tzq_engineer@163.com>
2018-06-12 09:53:01 +0800

    --entry - yarn --

---
+++ src/main/scala/entry/YARN.scala
@@ -0,0 +1,7 @@
+package entry
+
+/**
+  * Created by tianzhongqiu on 2018/5/28.
+  */
+case class YARN (userName :String,logType:String,opTime:String,operation:String,ip:String,targetName:String,
+                 result:String,discription:String,permisions:String,appID:String,containerID:String)
